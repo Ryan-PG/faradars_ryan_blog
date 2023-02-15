fe3345a26a5b5a6c4f5453f415e9e7da17abf2e7
Ryan Heida <heidarih313@gmail.com>
2023-02-16 00:03:49 +0330

    Added templates and base page render and extendation

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index') #www.ryan-blogs.com\
+  path('', views.index, name='index'),     #www.ryan-blogs.com\
+  path('about', views.about, name='about') #www.ryan-blogs.com\about
 ]
