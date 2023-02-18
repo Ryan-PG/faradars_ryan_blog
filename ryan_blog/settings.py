8d3b68862e11de0a74389c70a2d5b0c348d016c0
Ryan Heida <heidarih313@gmail.com>
2023-02-18 22:58:47 +0330

    Connected postgresSQL database to project

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'ryan_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ryanblog_DB',
+        'USER': 'postgres', 
+        'PASSWORD': 'hello', 
+        'HOST': 'localhost'
     }
 }
 
