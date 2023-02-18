4ac6e9ca7a8f24f54a395d0b2d9f0422d91b93f5
Ryan Heida <heidarih313@gmail.com>
2023-02-19 02:12:05 +0330

    Added Posts model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
+from bloggers.models import Blogger
 
-# Create your models here.
+class Post(models.Model):
+  blogger = models.ForeignKey(Blogger, on_delete=models.DO_NOTHING)
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  comments_cout = models.IntegerField(default=0)
+  likes_cout = models.IntegerField(default=0)
+  post_date = models.DateTimeField(default=datetime.now, blank=True)
+  is_published = models.BooleanField(default=True)
+  photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+
+  def __str__(self) -> str:
+    return self.title
