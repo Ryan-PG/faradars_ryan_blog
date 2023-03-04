bf030cbd5273e9bc723ffbdd70b84081f57595fb
Ryan Heida <heidarih313@gmail.com>
2023-03-05 01:08:13 +0330

    Added media folder and some data to models

--- ryan_blog/settings.py
+++ ryan_blog/settings.py
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Meida Folder
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
--- ryan_blog/urls.py
+++ ryan_blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')), #www.ryan-blog.com
     path('posts/', include('posts.urls')), #www.ryan-blog.com\posts\
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
