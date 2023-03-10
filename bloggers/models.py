33b4f8c344ceaa7ae190c049b34855591dc33bd8
Ryan Heida <heidarih313@gmail.com>
2023-03-10 23:30:37 +0330

    Added user to blogger model Added auto select login user as blogger Added blogger to readonly_fields in posts.admin Added just seeing logged in user posts for every user

--- bloggers/models.py
+++ bloggers/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Blogger(models.Model):
+  user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
   name = models.CharField(max_length=200)
   photo = models.ImageField(upload_to='photos/bloggers/%Y/%m/%d/')
   birth_date = models.DateField(default=datetime.now, blank=True)
--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from bloggers.models import Blogger
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,7 +9,19 @@ class PostAdmin(admin.ModelAdmin):
   list_editable = ['is_published']
   list_filter = ['blogger']
   search_fields = ['title', 'text', 'blogger__name']
-  readonly_fields = ['comments_count', 'likes_count']
+  readonly_fields = ['comments_count', 'likes_count', 'blogger']
   list_per_page = 20
+
+  def save_model(self, request, obj, form, change) -> None:
+    obj.blogger = Blogger.objects.filter(user=request.user).first()
+    return super().save_model(request, obj, form, change)
+  
+  def get_queryset(self, request):
+    qs = super(PostAdmin, self).get_queryset(request)
+
+    if request.user.is_superuser:
+      return qs
+    
+    return qs.filter(blogger__user=request.user)
 
 admin.site.register(Post, PostAdmin)
