33c49f393e51e4a55e6f6e5262012fad0de3e9a3
Ryan Heida <heidarih313@gmail.com>
2023-03-19 01:32:43 +0330

    Added functionality for registration page form

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from bloggers.models import Blogger
 
 def register(request):
   if request.method == 'POST':
-    # Register User
-    print('yes it is POST method')
-    messages.error(request, 'This is a test for error message.')
-    return redirect('register')
+    # Get form variables
+    name = request.POST['name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    # Check if password match
+    if password == password2:
+      # Check username
+      if User.objects.filter(username=username).exists():
+        messages.error(request, 'این نام کاربری در دسترس نیست.')
+        return redirect('register')
+      else:
+        # Check email
+        if User.objects.filter(email=email).exists():
+          messages.error(request, 'این ایمیل قبلا استفاده شده است.')
+          return redirect('register')
+        else:
+          # It's Good
+          user = User.objects.create_user(username=username, password=password, email=email, first_name=name)
+          
+          # Create blogger
+          blogger = Blogger.objects.create(user=user, name=name, email=email)
+
+          # Login after registration
+          auth.login(request, user)
+          messages.success(request, 'ثبت نام با موفقیت انجام شد.')
+
+          return redirect('index')          
+    else:
+      messages.error(request, 'رمز عبورهای وارد شده مطابق نیستند.')
+      return redirect('register')
   else:
     return render(request, 'accounts/register.html')
 
