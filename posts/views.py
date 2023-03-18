629c3dcb3ac622f21f82b9ccd36f281938cea65d
Ryan Heida <heidarih313@gmail.com>
2023-03-18 18:30:47 +0330

    Made posts cards dynamic

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-  return render(request, 'posts/posts.html')
+  posts_list = Post.objects.all()
 
-def post(request):
+  context = {
+    'posts': posts_list
+  }
+  
+  return render(request, 'posts/posts.html', context)
+
+def post(request, post_id:int):
   return render(request, 'posts/post.html')
 
 def search(request):
