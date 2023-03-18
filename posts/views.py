b0991223d681021d3cf6a86176f7ecf0b567411d
Ryan Heida <heidarih313@gmail.com>
2023-03-18 21:24:17 +0330

    Added search_page

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post
 
 def index(request):
   posts_list = Post.objects.order_by('-post_date').filter(is_published = True)
 
-  paginator = Paginator(posts_list, 3)
+  paginator = Paginator(posts_list, 6)
   page = request.GET.get('page')
   paged_posts_list = paginator.get_page(page)
 
@@ -26,4 +27,18 @@ def post(request, post_id:int):
   return render(request, 'posts/post.html', context)
 
 def search(request):
-  return render(request, 'posts/search.html')
+  posts = Post.objects.order_by('-post_date').filter(is_published = True)
+
+  if 'search_text' in request.GET:
+    search_text = request.GET['search_text']
+
+    if search_text:
+      posts = posts.filter(Q(title__icontains=search_text)
+                           | Q(text__icontains=search_text)
+                           | Q(blogger__name__icontains=search_text))
+
+  context = {
+    'posts': posts
+  }
+  
+  return render(request, 'posts/search.html', context)
